a27b4cdef5584c685dc604bb27a177cf716ab85c
Caio Abrahão <contato.caioabrahao@gmail.com>
2024-12-09 23:04:06 -0300

    changed card section

--- src/components/home/CardSection.vue
+++ src/components/home/CardSection.vue
@@ -16,9 +16,9 @@ export default {
     data() {
         return {
             cards: [
-                { id: 1, title: 'Card 1', description: 'Description for card 1' },
-                { id: 2, title: 'Card 2', description: 'Description for card 2' },
-                { id: 3, title: 'Card 3', description: 'Description for card 3' },
+                { id: 1, title: 'Created with VUE', description: 'This website is created with VUE js and Vite! Also using some GSAP for animations, cuz im just like that ;)' },
+                { id: 2, title: 'My first Framework', description: 'This is my first time creating a website with a framework' },
+                { id: 3, title: 'By my self', description: 'Im trying to learn this by myself, because I learn better that way.' },
             ]
         };
     }
@@ -28,21 +28,42 @@ export default {
 <style scoped>
 .card-section {
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
 
     height: 100vh;
     background-color: #090909;
+
+    gap: 64px
+}
+.card-section h2{
+    font-size: 3rem;
 }
 
 .cards {
     display: flex;
-    gap: 20px;
+    gap: 32px;
 }
 
 .card {
     border: 1px solid #ccc;
     padding: 10px;
     border-radius: 5px;
+    height: 16rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+
+    transition: background-color .3s;
+    width: 20rem;
+}
+.card p {
+    text-align: center
+}
+.card:hover {
+    background-color: rgba(241, 241, 241, .3);
 }
 </style>
